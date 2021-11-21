4375155a2dc4f92fa87a7131f17ae40f5a1585a6
Megan Sickinger <mesickinger100@gmail.com>
2021-11-20 20:31:32 -0600

    Made basic PlayerSetup layout: Labels for character name; text boxes for each player; ready buttons for each player
    Made Player class and initialized with name, cash, and answer (more attributes may be needed; if so, just add with get/set and constructor)

--- NameThatTune/Form2.Designer.cs
+++ NameThatTune/Form2.Designer.cs
@@ -28,20 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPlayer1Name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPlayer2Name = new System.Windows.Forms.TextBox();
+            this.btnPlayer1Ready = new System.Windows.Forms.Button();
+            this.btnPlayer2Ready = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnPlayer2Ready, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtPlayer2Name, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtPlayer1Name, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnPlayer1Ready, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.26804F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.2268F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.50515F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(594, 252);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(300, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Player 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPlayer1Name
+            // 
+            this.txtPlayer1Name.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPlayer1Name.Font = new System.Drawing.Font("Palatino Linotype", 15.75F);
+            this.txtPlayer1Name.Location = new System.Drawing.Point(3, 133);
+            this.txtPlayer1Name.Name = "txtPlayer1Name";
+            this.txtPlayer1Name.Size = new System.Drawing.Size(291, 36);
+            this.txtPlayer1Name.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(291, 47);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Character Name:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(300, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(291, 47);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Character Name:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPlayer2Name
+            // 
+            this.txtPlayer2Name.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPlayer2Name.Font = new System.Drawing.Font("Palatino Linotype", 15.75F);
+            this.txtPlayer2Name.Location = new System.Drawing.Point(300, 133);
+            this.txtPlayer2Name.Name = "txtPlayer2Name";
+            this.txtPlayer2Name.Size = new System.Drawing.Size(291, 36);
+            this.txtPlayer2Name.TabIndex = 5;
+            // 
+            // btnPlayer1Ready
+            // 
+            this.btnPlayer1Ready.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnPlayer1Ready.Font = new System.Drawing.Font("Palatino Linotype", 15.75F);
+            this.btnPlayer1Ready.Location = new System.Drawing.Point(3, 213);
+            this.btnPlayer1Ready.Name = "btnPlayer1Ready";
+            this.btnPlayer1Ready.Size = new System.Drawing.Size(291, 36);
+            this.btnPlayer1Ready.TabIndex = 6;
+            this.btnPlayer1Ready.Text = "Ready!";
+            this.btnPlayer1Ready.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayer2Ready
+            // 
+            this.btnPlayer2Ready.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnPlayer2Ready.Font = new System.Drawing.Font("Palatino Linotype", 15.75F);
+            this.btnPlayer2Ready.Location = new System.Drawing.Point(300, 213);
+            this.btnPlayer2Ready.Name = "btnPlayer2Ready";
+            this.btnPlayer2Ready.Size = new System.Drawing.Size(291, 36);
+            this.btnPlayer2Ready.TabIndex = 7;
+            this.btnPlayer2Ready.Text = "Ready!";
+            this.btnPlayer2Ready.UseVisualStyleBackColor = true;
             // 
             // frmPlayerSetup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(594, 327);
+            this.ClientSize = new System.Drawing.Size(594, 252);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "frmPlayerSetup";
             this.Text = "Player Setup";
             this.Load += new System.EventHandler(this.frmPlayerSetup_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtPlayer2Name;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPlayer1Name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPlayer2Ready;
+        private System.Windows.Forms.Button btnPlayer1Ready;
     }
 }
---
+++ NameThatTune/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameThatTune
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public decimal Cash { get; set; }
+
+        public string Answer { get; set; }
+        public Player(string name, decimal c, string a)
+        {
+            Name = name;
+            Cash = c;
+            Answer = a;
+        }
+    }
+    
+}
